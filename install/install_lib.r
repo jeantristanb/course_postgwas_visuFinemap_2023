bebb838fea57226729b631dfb41bf283007a3fc4
brandenburg <jeantristanb@free.fr>
2023-01-25 13:47:51 +0200

    update docker and install lib

--- install/install_lib.r
+++ install/install_lib.r
@@ -1,14 +1,23 @@
-install.packages('data.table', repos='cran.r-project.org/')
+
+
+
+## dependencie for devtools
+install.packages('https://cran.r-project.org/src/contrib/Archive/pbkrtest/pbkrtest_0.4-8.6.tar.gz')
 install.packages('devtools',  repos='cran.r-project.org/')
+
+install.packages('data.table', repos='cran.r-project.org/')
 install.packages('qqman',  repos='cran.r-project.org/')
 install.packages('ggplot2',  repos='cran.r-project.org/')
 install.packages("gridExtra",  repos='cran.r-project.org/')
+install.packages("optparse", repos='cran.r-project.org/')
 
 
 devtools::install_github('kaustubhad/fastman')
 devtools::install_github('anastasia-lucas/hudson')
 devtools::install_github('kassambara/ggpubr')
-# for 3.6
+
+# dependencie for checkmate
+install.packages("checkmate", repos='cran.r-project.org/')
 install.packages("https://cran.r-project.org/src/contrib/Archive/forestplot/forestplot_2.0.1.tar.gz")
 
 

d45fea4720e8f0bc62f4206e5d4c12b98075132a
jeantristanb <jeantristanb@free.fr>
2023-02-07 11:07:15 +0200

    update

--- excercice/Datamanagement/README.md
+++ excercice/Datamanagement/README.md
@@ -1,7 +1,10 @@
 # Data managment
-##Description data
-data can be found [here](https://www.dropbox.com/s/yqf9krvsu0fxu87/course_h3abionet_2023.gemma.gz
-gunzip course_h3abionet_2023.gemma.gz), is a full summary statistics as previous.
+## Description data
+
+Data can be found [here](https://www.dropbox.com/s/yqf9krvsu0fxu87/course_h3abionet_2023.gemma.gz) is a full summary statistics contains ~ 6 million positions .
+
+## Download data
+
 ```
 wget -c \
 https://www.dropbox.com/s/yqf9krvsu0fxu87/course_h3abionet_2023.gemma.gz
--- excercice/Finemapping/prepared_sumstat.r
+++ excercice/Finemapping/prepared_sumstat.r
@@ -5,19 +5,17 @@ datasumstat<-fread('data/data_regplot.gemma')
 databim<-fread('data/chr17_77727734_79727734.bim', header=F)
 ## we keep only positions in 
 datasumstatclean<-merge(datasumstat, databim[,c(1,4)],by.x=c('chr','ps'),by.y=c("V1","V4"), all=F)
-## 
-chro<-17
-pos<-78727734
-around=100000
-
+## defined variable to defined chromosome, position and positions around 
+chro<-17;pos<-78727734;around=100000
 # we selected positions around chr and pos
 datasumstatclean2<-datasumstatclean[datasumstatclean$chr==chro & datasumstatclean$ps>(pos-around) & datasumstatclean$ps<(pos+around),]
-#rsid chromosome position allele1 allele2 maf beta se
-#rs1 10 1 T C 0.35 0.0050 0.0208
 ## transformed af in maf
 datasumstatclean2$maf<-datasumstatclean2$af
 datasumstatclean2$maf[datasumstatclean2$af>0.5]<- 1 - datasumstatclean2$af[datasumstatclean2$af>0.5]
+# select column of interrest and change header
 datasumstatclean2<-datasumstatclean2[,c('rs', 'chr', 'ps', 'allele1', 'allele0', 'maf', 'beta','se')]
 names(datasumstatclean2)<-c('rsid', 'chromosome', 'position', 'allele1', 'allele2', 'maf', 'beta','se')
+# write new summary statistics
 write.table(datasumstatclean2, quote=F, row.names=F, col.names=T, file='data/sumstat.z',sep=' ')
+## build a bed file for plink and build LD
 write.table(datasumstatclean2[,c('chromosome', 'position', 'position','rsid')], quote=F, row.names=F, col.names=T, file='data/sumstat.bed',sep='\t')

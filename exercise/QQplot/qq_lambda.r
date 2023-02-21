3cc9480dc2dca048f7e9eaac5efe0e8308f78540
jeantristanb <jeantristanb@free.fr>
2023-02-21 10:54:53 +0200

    minor error

--- exercise/QQplot/qq_lambda.r
+++ exercise/QQplot/qq_lambda.r
@@ -5,5 +5,5 @@ data_sumstat<-fread('data/result.plink') # reading summary statistics
 chisq <- qchisq(1-data_sumstat$P[!is.na(data_sumstat$P)],1) # transform p in chisq using density
 lambda<-median(chisq)/qchisq(0.5,1) # computed lambda
 
-print(lamdba)
+print(lambda)
 
